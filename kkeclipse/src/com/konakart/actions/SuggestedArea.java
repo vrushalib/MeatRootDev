915ac85dfcd9c8975904b33f5c320af322f5e1c5
pooja <poojabihani11@gmail.com>
2014-08-24 23:05:51 -0400

    fixed suggested area exception

--- kkeclipse/src/com/konakart/actions/SuggestedArea.java
+++ kkeclipse/src/com/konakart/actions/SuggestedArea.java
@@ -25,7 +25,7 @@ public class SuggestedArea extends BaseAction
         	System.out.println(pinCode+":"+emailId);
             if ( pinCode != null && !pinCode.equals("") && emailId != null && !emailId.equals(""))
             {
-        		com.konakart.db.KKBasePeer.executeStatement("insert into suggested_areas (emailid, pincode) values ('"+emailId+"', '"+pinCode+"');", org.apache.torque.Torque.getConnection("store1", "root", "root123"));
+        		com.konakart.db.KKBasePeer.executeStatement("insert into suggested_areas (emailid, pincode) values ('"+emailId+"', '"+pinCode+"');", org.apache.torque.Torque.getConnection("store1", "root", "pass123"));
                 return SUCCESS;
             }
 
